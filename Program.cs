6d74e25fc4a2de1d6001ff13ac92830e618337da
IcyAuk <r.nvessh@gmail.com>
2026-02-05 22:28:51 +0100

    added HTTP GET & HTTP POST endpoints

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ app: web app object
     delegate is a function/method stored in a var.
 
 */
-app.MapGet("todoitems/complete",async(TodoDb db) =>
+app.MapGet("/todoitems/complete",async(TodoDb db) =>
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
 /*
@@ -24,10 +24,45 @@ Async Lamba expression, pattern matching, terniary operator
 Pattern Matching is Todo todo checks if .FindAsync() returns non null Todo
 if true then assign 
 */
-app.MapGet("todoitems/{id}",
+app.MapGet("/todoitems/{id}",
     async(int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
     is Todo todo
     ? Results.Ok(todo)
     : Results.NotFound());
 
+app.MapPost("/todoitems",async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    
+    return Results.Created($"/todoitems/{todo.Id}",todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db)=>
+{
+   var todo = await db.Todos.FindAsync(id);
+
+   if (todo is null) return Results.NotFound();
+
+   todo.Name = inputTodo.Name;
+   todo.IsComplete = inputTodo.IsComplete; 
+
+   await db.SaveChangesAsync();
+
+   return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+   if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    } 
+
+    return Results.NotFound();
+});
+
+app.Run();
